778fab5890e0ea0c447274d2bd2b22367785b6bf
aw <aw@MacBook-Pro-aw.HomeLAN>
2023-07-06 18:24:13 +0300

    Commands

--- app/User/Application/CommandHandlers/RegisterUserHandler.php
+++ app/User/Application/CommandHandlers/RegisterUserHandler.php
@@ -2,7 +2,34 @@
 
 namespace App\User\Application\CommandHandlers;
 
+use App\SharedKernel\Domain\Services\GuidGenerator;
+use App\SharedKernel\Domain\ValueObjects\EmailAddress;
+use App\User\Application\Commands\RegisterUser;
+use App\User\Application\Services\PasswordHasher;
+use App\User\Domain\Entity\User;
+use App\User\Domain\Repositories\UserRepository;
+use App\User\Domain\ValueObjects\UserName;
+use App\User\Domain\ValueObjects\UserPassword;
+
 class RegisterUserHandler
 {
+    public function __construct(
+        private readonly UserRepository $repository,
+        private readonly PasswordHasher $hasher,
+        private readonly GuidGenerator $guid
+    )
+    {
+    }
 
+    public function __invoke(RegisterUser $command): string
+    {
+        $user = User::register(
+            $this->guid->next(),
+            UserName::create($command->name),
+            EmailAddress::create($command->email),
+            UserPassword::create($this->hasher->hash($command->password))
+        );
+        $this->repository->save($user);
+        return $user->getId()->value();
+    }
 }
