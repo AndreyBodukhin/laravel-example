2f8083df8972b5b133d0fe6eda153e374e995887
aw <aw@MacBook-Pro-aw.local>
2023-07-10 07:32:07 +0300

    DI

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,21 +2,25 @@
 
 namespace App\Providers;
 
+use App\SharedKernel\Infrastructure\Repositories\EloquentEventStore;
+use App\SharedKernel\Infrastructure\Repositories\EventStore;
+use App\User\Domain\Repositories\UserRepository;
+use App\User\Infrastructure\Domain\Repositories\EventSourcingUserRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
-        //
+        $this->app->singleton(EventStore::class, EloquentEventStore::class);
+        $this->app->singleton(
+            UserRepository::class,
+            fn($app) => new EventSourcingUserRepository(
+                $app->make(EventStore::class)
+            )
+        );
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         //
